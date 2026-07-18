550b147b371c8f7734f0a5280bd4b8904ed5b829
Lemon2909 <arjunnagar2909@gmail.com>
2026-07-18 18:42:19 +0530

    push

--- src/vector2.cpp
+++ src/vector2.cpp
@@ -1,5 +1,11 @@
-class vector2 {
+class Vector2 {
     double x; 
     double y;
-    
+    Vector2 (double x, double y) {
+        this->x = x;
+        this->y = y;
+    }
+    Vector2 operator+(const Vector2& b) const {
+        return Vector2(x + b.x, y + b.y);
+    }
 }
